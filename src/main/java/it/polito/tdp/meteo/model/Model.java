53f52f04898ee5aa681889cecf1722b93ea87d0e
david <david@Razer15.lan>
2022-04-14 11:04:56 +0200

    Solved

--- src/main/java/it/polito/tdp/meteo/model/Model.java
+++ src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -57,10 +57,17 @@ public class Model {
 
 	private double calcolaCosto(List<Citta> parziale) {
 		double costo = 0.0;
-		int i = 0;
-		for (Citta c : parziale) {
-			costo += c.getRilevamenti().get(i++).getUmidita();
-			if (c.getNome().equals(parziale.get(i - 1).getNome()) == false) {
+		for (int giorno = 1; giorno <= NUMERO_GIORNI_TOTALI; giorno++) {
+			// citta in cui cazzo sono
+			Citta c = parziale.get(giorno - 1);
+			//umidita nella citta in cui sono
+			double umid = c.getRilevamenti().get(giorno - 1).getUmidita();
+			costo += umid;
+		}
+		//vedo quante volte cambio la city nei 15gg
+		for (int giorno = 2; giorno <= NUMERO_GIORNI_TOTALI; giorno++) {
+			//se cambio citta mi aggiunge i 100
+			if (!parziale.get(giorno - 1).equals(parziale.get(giorno - 2))) {
 				costo += COST;
 			}
 		}
@@ -68,33 +75,37 @@ public class Model {
 	}
 
 	private boolean isValid(List<Citta> parziale, Citta prova) {
-		//verifica giorni massimi
-		//contiamo quante volte la città 'prova' era già apparsa nell'attuale lista costruita fin qui
+		// verifica giorni massimi
+		// contiamo quante volte la città 'prova' era già apparsa nell'attuale lista
+		// costruita fin qui
 		int conta = 0;
 		for (Citta c : parziale) {
 			if (c.equals(prova)) {
 				conta++;
 			}
 		}
-		//se supero giorni max sono out
+		// se supero giorni max sono out
 		if (conta >= NUMERO_GIORNI_CITTA_MAX) {
 			return false;
 		}
 		// verifica dei giorni minimi
-		if (parziale.size() == 0) { //primo giorno posso inserire qualsiasi città
+		if (parziale.size() == 0) { // primo giorno posso inserire qualsiasi città
 			return true;
 		}
 		if (parziale.size() == 1 || parziale.size() == 2) {
-			//siamo al secondo o terzo giorno, non posso cambiare
-			//quindi l'aggiunta è valida solo se la città di prova coincide con la sua precedente
+			// siamo al secondo o terzo giorno, non posso cambiare
+			// quindi l'aggiunta è valida solo se la città di prova coincide con la sua
+			// precedente
 			return parziale.get(parziale.size() - 1).equals(prova);
 		}
-		//nel caso generale, se ho già passato i controlli sopra, non c'è nulla che mi vieta di rimanere nella stessa città
-		//quindi per i giorni successivi ai primi tre posso sempre rimanere
+		// nel caso generale, se ho già passato i controlli sopra, non c'è nulla che mi
+		// vieta di rimanere nella stessa città
+		// quindi per i giorni successivi ai primi tre posso sempre rimanere
 		if (parziale.get(parziale.size() - 1).equals(prova)) {
 			return true;
 		}
-		// se cambio città mi devo assicurare che nei tre giorni precedenti sono rimasto fermo 
+		// se cambio città mi devo assicurare che nei tre giorni precedenti sono rimasto
+		// fermo
 		if (parziale.get(parziale.size() - 1).equals(parziale.get(parziale.size() - 2))
 				&& parziale.get(parziale.size() - 2).equals(parziale.get(parziale.size() - 3))) {
 			return true;
